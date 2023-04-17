c6827bebc07de9eba85ecf288bb8a2a87160cc29
shavit shelli <shavit.shelli57@gmail.com>
2023-04-17 20:40:08 +0300

    Part 6 commit

--- Hello.java
+++ Hello.java
@@ -4,7 +4,7 @@ public class Hello
     {
         for(int i=1; i<=10; i++)
         {
-            System.out.println("hello world "+i);
+            System.out.println("hello my beautiful world "+i);
         }
     }
 }
